d4e8d592547560cdb54c4b114df4dfdf4aa8ba08
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-08-24 09:36:29 +0300

    Added Query param route

--- api.py
+++ api.py
@@ -18,5 +18,13 @@ def find_students():
     return students
 
 @app.get('/api/students/{id}')
-def find_student(id: int = Path(..., description="The student id", gt=0)):
+def find_student_by_id(id: int = Path(..., description="The student id", gt=0)):
     return next((student for student in students if student["id"] == id), None)
+
+@app.get('/api/student')
+def find_student_by_name(name: str):
+    for student in students:
+        if student["name"] == name:
+            return student
+
+    return {"Error": "Student not found"}
